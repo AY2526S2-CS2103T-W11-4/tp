6a0d946cca149188a2deb6208443b8942fad9945
HoneyGlazedPorkChops <eugeneong3@gmail.com>
2026-03-14 18:03:25 +0800

    Add Delivery package and respective classes

---
+++ src/main/java/seedu/address/model/delivery/ProductContainsKeywordsPredicate.java
@@ -0,0 +1,43 @@
+package seedu.address.model.delivery;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.commons.util.ToStringBuilder;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Tests that a {@code Delivery}'s {@code Product} matches any of the keywords given.
+ */
+public class ProductContainsKeywordsPredicate implements Predicate<Delivery> {
+    private final List<String> keywords;
+
+    public ProductContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Delivery delivery) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(delivery.getProduct().productName, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ProductContainsKeywordsPredicate)) {
+            return false;
+        }
+
+        ProductContainsKeywordsPredicate otherProductContainsKeywordsPredicate = (ProductContainsKeywordsPredicate) other;
+        return keywords.equals(otherProductContainsKeywordsPredicate.keywords);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keywords", keywords).toString();
+    }
+}
